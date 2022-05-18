0e47590036bba805743778f4148418972206ad28
Upsided <matthewlloydw@gmail.com>
2022-05-18 01:10:13 +0000

    first commit

---
+++ source/index.ts
@@ -0,0 +1,16 @@
+type main = {
+    /**
+ * The version of the library.
+ */
+    version: '1.0.0'
+}
+
+const reggie: main = {
+    version: '1.0.0'
+}
+
+const { version } = reggie
+
+export { version }
+
+export default reggie
