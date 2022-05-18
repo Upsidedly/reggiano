e4d791cf4ffd10f38f961463ffe566d8d801506c
Upsided <matthewlloydw@gmail.com>
2022-05-18 03:23:17 +0000

    Shallowcap addition

--- source/index.ts
+++ source/index.ts
@@ -2,11 +2,18 @@ type main = {
     /**
  * The version of the library.
  */
-    version: '1.0.0'
+    version: '1.0.0',
+    /**
+     * Shallow capitalization of the provided string
+     * @param {string} text The string to be capitalized
+     * @returns {string} The capitalized string
+     */
+    shallowcap: (text: string) => string,
 }
 
 const reggie: main = {
-    version: '1.0.0'
+    version: '1.0.0',
+    shallowcap: (text: string) => text.charAt(0).toUpperCase() + text.slice(1),
 }
 
 const { version } = reggie
